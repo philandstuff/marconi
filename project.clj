2259e505066428ea0e3d774f1e855fddfe6f9620
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-08-03 00:50:22 +0100

    add noderepl to project.clj

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[core.async "0.1.0-SNAPSHOT"]]
-  :plugins [[lein-cljsbuild "0.3.2"]]
+  :dependencies [[core.async "0.1.0-SNAPSHOT"]
+                 [org.bodil/cljs-noderepl "0.1.10"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :plugins [[lein-cljsbuild "0.3.2"]
+            [org.bodil/lein-noderepl "0.1.10"]]
   :cljsbuild {:builds
               [{:source-paths ["src"],
                 :compiler {:output-to "js/main.js",
