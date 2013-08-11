52b0e46ae136eba42fedae1894d0b17bab1bf98a
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2013-08-11 16:20:31 +0100

    dump statsd and redis stuff in outputs.
    doesn't currently work with new config style til i get round to fixing
    that

--- src/marconi/core.cljs
+++ src/marconi/core.cljs
@@ -6,41 +6,7 @@
             [marconi.inputs :as input]
             [marconi.outputs :as output]))
 
-(def dgram (node/require "dgram"))
-(def redis (node/require "redis"))
 (def fs (node/require "fs"))
-
-(defn redis-channel []
-  (let [ch       (async/chan)
-        retry-ch (async/chan)
-        client   (.createClient redis 6379 "127.0.0.1"
-                                (doto (js-obj)
-                                  (aset "no_ready_check" true)))]
-    (.on client "error" (fn [err]
-                          (.log js/console (str "Error " err))))
-    (go (while true
-          (>! retry-ch [0 (<! ch)])))
-    (go
-     ;; TODO: close connection
-     ;; TODO: react to (.on client "end)
-     (while true
-       (let [[attempt msg] (<! retry-ch)]
-         (if (>= attempt 3)
-           (.log js/console (str "Too many attempts, dropping" msg))
-           (.lpush client "logs" msg (fn [err result]
-                                       (when err
-                                         (.log js/console (str "Error" err ", retrying msg" msg))
-                                         (async/put! retry-ch [(inc attempt) msg]))))))))
-    ch))
-
-(defn statsd-channel []
-  (let [statsd-ch (async/chan)
-        socket    (.createSocket dgram "udp4")]
-    (go
-     (while true
-       (let [statsd-packet (<! statsd-ch)]
-         (.send socket statsd-packet 0 (.-length statsd-packet) 8125 "127.0.0.1"))))
-    statsd-ch))
 
 (defn read-config [config-filename]
   (let [ch (async/chan)]
--- src/marconi/outputs.cljs
+++ src/marconi/outputs.cljs
@@ -10,3 +10,39 @@
           (when-let [event (<! stdout-ch)]
             (.write stdout (JSON/stringify event)))))
     stdout-ch))
+
+;; dumping ground for stuff that doesn't work atm
+(def dgram (node/require "dgram"))
+(def redis (node/require "redis"))
+
+(defn redis-channel []
+  (let [ch       (async/chan)
+        retry-ch (async/chan)
+        client   (.createClient redis 6379 "127.0.0.1"
+                                (doto (js-obj)
+                                  (aset "no_ready_check" true)))]
+    (.on client "error" (fn [err]
+                          (.log js/console (str "Error " err))))
+    (go (while true
+          (>! retry-ch [0 (<! ch)])))
+    (go
+     ;; TODO: close connection
+     ;; TODO: react to (.on client "end)
+     (while true
+       (let [[attempt msg] (<! retry-ch)]
+         (if (>= attempt 3)
+           (.log js/console (str "Too many attempts, dropping" msg))
+           (.lpush client "logs" msg (fn [err result]
+                                       (when err
+                                         (.log js/console (str "Error" err ", retrying msg" msg))
+                                         (async/put! retry-ch [(inc attempt) msg]))))))))
+    ch))
+
+(defn statsd-channel []
+  (let [statsd-ch (async/chan)
+        socket    (.createSocket dgram "udp4")]
+    (go
+     (while true
+       (let [statsd-packet (<! statsd-ch)]
+         (.send socket statsd-packet 0 (.-length statsd-packet) 8125 "127.0.0.1"))))
+    statsd-ch))
